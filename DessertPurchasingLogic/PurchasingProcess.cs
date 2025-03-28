7a98b8911325c70ebeda9a68abbb20a3bd8b395a
BalawangJb <jbbalawang262005@gmail.com>
2025-03-28 20:24:07 +0800

    separate business Logic

---
+++ DessertPurchasingLogic/PurchasingProcess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DessertPurchasingLogic
+{
+    public class PurchasingProcess
+    {
+       static String userName = "user1";
+        static   String uPass = "0000";
+        
+       
+        public static int purchasedItem(int payment, String[] flavors, int[] price, int choice ,int quantity)
+        {
+
+            int totalPrice = price[choice - 1] * quantity;
+
+            if (payment >= totalPrice)
+            {
+                payment -= totalPrice;
+
+                return payment;
+            }
+            return -1;
+        }
+        public static bool validateLogin(String name,String pass)
+        {
+           
+                return name == userName && pass == uPass;
+            }
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using DessertPurchasingLogic;
 namespace Dessertsjb
 
 {
@@ -7,65 +7,105 @@ namespace Dessertsjb
     {
         static void Main(string[] args)
         {
-            String[] flavors = { "Classic", "Cookis n Cream", "Rocky Road" };
-            int[] price = { 20, 25, 25 };
-            Console.WriteLine("Welcome to Graham Bar to Go");
 
+
+
+
+            String[] flavors = { "Balck Forest", "Pistaccio", "Red Velvet", "Classic Bavarian", "ChocoButternut", "HoneyGlazed" };
+            int[] price = { 45, 60, 60, 30, 35, 40 };
+            Console.WriteLine("Arise Dessert Shop");
+            Console.WriteLine("--------------");
+
+            String name;
+            String pass;
+            do
+            {
+                Console.WriteLine("Enter Username");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter Password");
+                pass = Console.ReadLine();
+              
+                if (!PurchasingProcess.validateLogin(name, pass))
+                {
+                    Console.WriteLine("Failed: Incorrect Username or Password. Please try again.");
+                }
+            } while (!PurchasingProcess.validateLogin(name, pass));
+
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Welcome to Arise Dessert Shop {name}");
+            
             //method calling for payment
+
             int payment = getPayment();
+
+            {
+
+            }
             while (true)
             {
 
 
                 //method calling for flavors
-                displayFlavors();
-                Console.Write("Flavor :");
+                displayFlavorsCake();
+                displayFlavorsDonut();
+                Console.Write("Choose from 1-6 or 0 to exit :");
                 int choice = Convert.ToInt32(Console.ReadLine());
+                
                 switch (choice)
                 {
                     case 1:
                     case 2:
                     case 3:
-                        Console.WriteLine($"You select {flavors[choice - 1]}");
-                        Console.Write("Enter Quantity:");
-                        int quantity = Convert.ToInt16(Console.ReadLine());
-                        int totalPrice = price[choice - 1] * quantity;
-                        if (payment >= totalPrice)
+                    case 4:
+                    case 5:
+                    case 6:
+                        Console.WriteLine("Enter Quantity");
+
+                        int quantity = Convert.ToInt32(Console.ReadLine());
+                        int newBalance = PurchasingProcess.purchasedItem(payment, flavors, price, choice,quantity);
+                        
+                      
+                        if(newBalance >= 0)
                         {
-                            payment -= totalPrice;
-                            Console.WriteLine($"You Bought {quantity} of {flavors[choice - 1]}");
-                            Console.WriteLine($"Your total cost is {totalPrice}");
-                            Console.WriteLine($"Your Balance is {payment}");
+                            payment = newBalance;
+                            Console.WriteLine($"You bought {quantity} of {flavors[choice - 1]}.");
+                            Console.WriteLine($"Total cost: {price[choice - 1] * quantity} PHP.");
+                            Console.WriteLine($"Available balance: {newBalance} PHP.");
+                            Console.WriteLine("");
+                           
                         }
                         else
                         {
-                            Console.WriteLine("Insufficient Balance, Please Select the right Quantity.");
+                            Console.WriteLine("Insufficient Balance, Please enter a smaller quantity or other item");
+                            Console.WriteLine("--------------");
+                            Console.WriteLine("");
+                        }
 
-                        }
-                        if (payment < 20)
-                        {
-                            Console.WriteLine("You Dont have enough money to buy more.");
-                            Console.WriteLine($"Thank u for purchasing!");
-                            return;
-                        }
                         break;
                     case 0:
-                        Console.WriteLine($"Thank u for purchasing!");
-                        return;
+                                Console.WriteLine($"Thank u for purchasing!");
+                                return;
 
-                    default:
-                        Console.WriteLine("Invalid, Please enter a valid option");
-                        break;
-                }
+                            default:
+                                Console.WriteLine("Invalid, Please enter a valid option");
+                                break;
+                            }
             }
         }
         //method to display or to call the flavors available
-        static void displayFlavors()
+        static void displayFlavorsCake()
         {
-            Console.WriteLine("Flavors");
-            Console.WriteLine("[1]Classic 20 php");
-            Console.WriteLine("[2]Cookis n Cream 25 php");
-            Console.WriteLine("[3]Rocky Road 25 php");
+            Console.WriteLine("Cake Sliced");
+            Console.WriteLine("[1]Black Forest 45 php");
+            Console.WriteLine("[2]Pistaccio 60 php");
+            Console.WriteLine("[3]Red Velvet 60 php");
+            Console.WriteLine("---------");
+        }
+        static void displayFlavorsDonut()
+        {
+            Console.WriteLine("[4]Classic Bavarian 30 php ");
+            Console.WriteLine("[5]Choco Butternut 35php");
+            Console.WriteLine("[6]Honey Glazed 40php");
             Console.WriteLine("[0] to exit");
             Console.WriteLine("---------");
         }
@@ -75,22 +115,54 @@ namespace Dessertsjb
             int payment;
             do
             {
+                Console.WriteLine("");
                 Console.Write("Enter your Money:");
-                payment = Convert.ToInt32(Console.ReadLine());
+                String input = Console.ReadLine()??"";
                 Console.WriteLine();
+                
 
-                if (payment < 20)
+                if (!int.TryParse(input,out payment))
 
                 {
-                    Console.WriteLine("You dont have enough money to buy");
-
+                    Console.WriteLine("Invalid Input, Please Input Money");
+                    continue;
                 }
-            } while (payment < 20);
+                if(payment<30)
+                {
+                    Console.WriteLine("You Dont have Enough Balance");
+                }
+            } while (payment < 30);
             return payment;
         }
+       
 
+        //static int purchasedItem(int payment, String[] flavors, int[] price, int choice)
+        //{
+        //    Console.WriteLine("Enter Quantity");
+        //    int quantity = Convert.ToInt32(Console.ReadLine());
+        //    int totalPrice = price[choice - 1] * quantity;
+
+        //    if (payment >= totalPrice)
+        //    {
+        //        payment -= totalPrice;
+        //        Console.WriteLine($"You Bought {quantity} of {flavors[choice - 1]}");
+        //        Console.WriteLine($"Your total Cost is {totalPrice}");
+        //        Console.WriteLine($"Your available balance is {payment}");
+
+
+        //        if (payment < 30)
+        //        {
+        //            Console.WriteLine("You cannot purchased more, thank u for buying");
+
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Insufficient Balance Please Select the right Item or Quantity");
+        //    }
+
+        //        return payment;
+        //}
     }
-
 }
-
-
+    
